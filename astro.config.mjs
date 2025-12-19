7116b39c5feb6ecade74cc9baf7820627112cf48
Bishal Biswas <bbiswas47@outlook.com>
2025-12-19 17:08:00 +0530

    CodeMastery initital commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap()],
+});
